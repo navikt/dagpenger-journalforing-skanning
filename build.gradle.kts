c0b7282d3f9d4f20aeefdb0a496e33655ad414d3
Geir André Lund <geir.andre.lund@nav.no>
2018-12-10 12:25:22 +0100

    Update to gradle 5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,7 @@ val log4j2Version = "2.11.1"
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("no.nav.dagpenger:streams:0.2.2-SNAPSHOT")
-    implementation("no.nav.dagpenger:events:0.1.6-SNAPSHOT")
+    implementation("no.nav.dagpenger:events:0.1.9-SNAPSHOT")
 
     implementation("io.github.microutils:kotlin-logging:$kotlinLoggingVersion")
     implementation("com.github.kittinunf.fuel:fuel:$fuelVersion")
