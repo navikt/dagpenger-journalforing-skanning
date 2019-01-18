c7d06dff646fea4c17e4434ac39af1437bd727a1
Geir André Lund <geir.andre.lund@nav.no>
2019-01-18 08:59:31 +0100

    Bump events to no.nav.dagpenger:events:0.2.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("no.nav.dagpenger:streams:0.2.5-SNAPSHOT")
     implementation("no.nav.dagpenger:dagpenger-metrics:1.0-SNAPSHOT")
-    implementation("no.nav.dagpenger:events:0.2.0-SNAPSHOT")
+    implementation("no.nav.dagpenger:events:0.2.1-SNAPSHOT")
 
     implementation("io.github.microutils:kotlin-logging:$kotlinLoggingVersion")
     implementation("com.github.kittinunf.fuel:fuel:$fuelVersion")
