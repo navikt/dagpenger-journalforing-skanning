5a64a365e3a9e87d507dfb0df0c625c7e20aaa7e
Geir André Lund <geir.andre.lund@nav.no>
2019-03-13 14:59:57 +0100

    confluent version to 5.0.2 kafka clients to 2.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ java {
 
 val kotlinLoggingVersion = "1.6.22"
 val fuelVersion = "1.15.0"
-val kafkaVersion = "2.0.0"
+val kafkaVersion = "2.0.1"
 val confluentVersion = "4.1.2"
 val ktorVersion = "1.0.0"
 val prometheusVersion = "0.6.0"
